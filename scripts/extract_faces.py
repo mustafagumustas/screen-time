3c00914b7e6df1a008271c970afcfd3637fc8151
mustafagumustas <mustafagumustas6@gmail.com>
2023-04-23 17:41:08 +0300

    eye nose triangle made, trying to find the direction of rotation

--- scripts/extract_faces.py
+++ scripts/extract_faces.py
@@ -1,45 +1,101 @@
-import numpy as np
 import cv2
+import dlib
+import numpy as np
 
+# landmarks locations in their list
+# The mouth can be accessed through points [48, 68].
+# The right eyebrow through points [17, 22].
+# The left eyebrow through points [22, 27].
+# The right eye using [36, 42].
+# The left eye with [42, 48].
+# The nose using [27, 35].
+# And the jaw via [0, 17].
 
-# face recognition, both frontal and lateral angels
-weights = "models/face_detection_yunet_2022mar.onnx"
-face_detector = cv2.FaceDetectorYN_create(weights, "", (1280, 720))
+# Initialize face detector and shape predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
+# Initialize webcam
 cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
 
+while True:
+    # Read a frame from the webcam
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    _, faces = face_detector.detect(frame)
-    faces = faces if faces is not None else []
+    # Detect faces in the grayscale frame
+    faces = detector(gray)
+
+    # Loop over the detected faces
     for face in faces:
-        box = list(map(int, face[:4]))
-        cv2.rectangle(frame, box, (0, 0, 255), 2, cv2.LINE_AA)
+        # Get the landmarks for the face in this frame
+        landmarks = predictor(gray, face)
 
-        # Draw landmarks
-        landmarks = list(map(int, face[4 : len(face) - 1]))
-        landmarks = np.array_split(landmarks, len(landmarks) / 2)
-        for landmark in landmarks:
-            radius = 5
-            thickness = -1
-            cv2.circle(
-                frame, tuple(landmark), radius, (0, 255, 0), thickness, cv2.LINE_AA
-            )
+        # Convert the landmarks to a NumPy array
+        landmarks = np.array([[p.x, p.y] for p in landmarks.parts()])
 
-    cv2.imshow("frame", frame)
+        # because we flip the frame left and right also flipped
+        right_eye = landmarks[42:48]
+        left_eye = landmarks[36:42]
+        nose = landmarks[27:35]
+
+        left_eye_center = np.mean(left_eye, axis=0).astype(int)
+        right_eye_center = np.mean(right_eye, axis=0).astype(int)
+        nose_center = np.mean(nose, axis=0).astype(int)
+
+        # ----------------------
+        # find rotation direction
+        if left_eye_center[1] > right_eye_center[1]:
+            point_3rd = left_eye_center[0], right_eye_center[1]
+            direction = -1  # rotate same direction to clock
+            print("rotate to clock direction")
+        else:
+            point_3rd = right_eye_center[1], left_eye_center[1]
+            direction = 1  # rotate inverse direction of clock
+            print("rotate to inverse clock direction")
+
+        # ----------------------
+
+        # cv2.circle(frame, point_3rd, 2, (255, 0, 0), 2)
+        # Draw the bounding box and landmarks for the detected face
+        cv2.rectangle(
+            frame,
+            (face.left(), face.top()),
+            (face.right(), face.bottom()),
+            (0, 0, 255),
+            2,
+        )
+        # cv2.line(frame, right_eye_center, left_eye_center, (67, 67, 67), 1)
+        # cv2.line(frame, left_eye_center, point_3rd, (67, 67, 67), 1)
+        # cv2.line(frame, right_eye_center, point_3rd, (67, 67, 67), 1)
+        # draw a triangle between eyes and nose centers
+
+        cv2.line(
+            frame,
+            left_eye_center,
+            right_eye_center,
+            (0, 255, 0),
+            thickness=3,
+            lineType=8,
+        )
+        cv2.line(
+            frame, left_eye_center, nose_center, (0, 255, 0), thickness=3, lineType=8
+        )
+        cv2.line(
+            frame, right_eye_center, nose_center, (0, 255, 0), thickness=3, lineType=8
+        )
+        # for landmark in landmarks:
+        #     cv2.circle(frame, tuple(landmark), 2, (0, 255, 0), -1)
+
+    # Display the frame with bounding boxes and landmarks
+    cv2.imshow("Face Detection", frame)
+
+    # Exit the loop if 'q' is pressed
     if cv2.waitKey(1) == ord("q"):
         break
 
-# When everything done, release the capture
+# Release the webcam and destroy all windows
 cap.release()
 cv2.destroyAllWindows()
