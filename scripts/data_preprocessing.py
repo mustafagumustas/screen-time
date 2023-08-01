d47f3327efdd62ef435fc1c4cac3107494c8c338
mustafagumustas <mustafagumustas6@gmail.com>
2023-08-01 14:27:43 +0300

    perfect preprocessing so far, covers augmentation with error handling

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,151 +1,136 @@
+import os
 import cv2
 import dlib
 import numpy as np
-import os
-from pillow_heif import register_heif_opener
-from typing import List
 from PIL import Image
-import tensorflow as tf
+from pillow_heif import register_heif_opener
+from keras.preprocessing.image import ImageDataGenerator
+from cv2 import FaceDetectorYN_create
+from tqdm import tqdm
+
+# Register HEIF opener for image conversion
+register_heif_opener()
 
 
-def convert_heic_to_png(filepath: str) -> np.ndarray:
-    register_heif_opener()
-    print(f"Converting file: {filepath}")
-    image = Image.open(filepath)
-    return np.array(image)
+def convert_heic_to_jpg(filepath: str) -> np.ndarray:
+    try:
+        image = Image.open(filepath)
+        jpg_path = os.path.splitext(filepath)[0] + ".jpg"
+        image.save(jpg_path)
+        return jpg_path
+    except Exception as e:
+        print(f"Error converting {filepath} to JPG: {e}")
+        return None
 
 
-def load_images(directory: str) -> List[np.ndarray]:
-    images = []
-    for filename in os.listdir(directory):
-        filepath = os.path.join(directory, filename)
-        if filepath.startswith(".DS_Store"):
-            continue
-        if filepath.lower().endswith(".heic"):
-            image = convert_heic_to_png(filepath)
-        else:
-            image = cv2.imread(filepath)
-            print(f"Image read: {filepath}")
-        if image is None:
-            print(f"Unable to read image: {filename}")
-            continue
-        image = np.array(image, dtype=np.float32) / 255.0
-        images.append(image)
-    return images
+def resize_image(image_path: str, target_size=(224, 224)) -> np.ndarray:
+    try:
+        img = cv2.imread(image_path)
+        if img is None:
+            print(f"Error reading image: {image_path}")
+            return None
+
+        resized_img = cv2.resize(img, target_size)
+        if resized_img is None:
+            print(f"Error resizing image: {image_path}")
+            return None
+
+        return resized_img
+    except Exception as e:
+        print(f"Error processing image: {image_path}: {e}")
+        return None
 
 
-def get_aligned_faces(directory: str, predictor_path: str) -> tuple:
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor(predictor_path)
+def align_faces_in_directory(directory: str, yunet_model_path: str) -> tuple:
+    # Load the YUNET face detector model
+    face_detector = FaceDetectorYN_create(yunet_model_path, "", (0, 0))
 
     aligned_faces = []
     image_names = []
     no_face_images = []
     no_face_image_names = []
-    images = load_images(directory)
 
-    for i, image in enumerate(images):
+    for filename in os.listdir(directory):
+        if filename.startswith(".DS_Store"):
+            continue
+        filepath = os.path.join(directory, filename)
+
+        # Convert HEIC to JPG if necessary
+        if filepath.lower().endswith(".heic"):
+            filepath = convert_heic_to_jpg(filepath)
+            if filepath is None:
+                continue
+
+        # Resize the image to the required dimensions
+        image = resize_image(filepath, target_size=(224, 224))
         if image is None:
-            print(f"Skipping face alignment for image {os.listdir(directory)[i]}")
-            no_face_images.append(None)
-            no_face_image_names.append(os.listdir(directory)[i])
             continue
 
-        original_image = image.copy()
-        no_faces = True
-        for _ in range(4):
-            dets = detector(image, 1)
-            if len(dets) > 0:
-                no_faces = False
-                for k, d in enumerate(dets):
-                    shape = predictor(image, d)
-                    face_chip = dlib.get_face_chip(image, shape)
-                    aligned_faces.append(face_chip)
-                    image_names.append(os.listdir(directory)[i])
-                break
-            else:
-                image = cv2.rotate(original_image, cv2.ROTATE_90_CLOCKWISE)
+        # Detect faces using YUNET
+        height, width, _ = image.shape
+        face_detector.setInputSize((width, height))
+        _, faces = face_detector.detect(image)
+        faces = faces if faces is not None else []
 
-        if no_faces:
-            print(f"No faces found in image {os.listdir(directory)[i]}")
-            no_face_images.append(None)
-            no_face_image_names.append(os.listdir(directory)[i])
+        if len(faces) > 0:
+            for face in faces:
+                box = list(map(int, face[:4]))
+                aligned_face = image[box[1] : box[1] + box[3], box[0] : box[0] + box[2]]
+                aligned_faces.append(aligned_face)
+                image_names.append(filename)
+        else:
+            print(f"No faces found in image: {filename}")
+            no_face_images.append(image)
+            no_face_image_names.append(filename)
 
     print(f"Aligned {len(aligned_faces)} faces and labeled {len(image_names)} faces.")
     print(f"Detected {len(no_face_images)} images with no faces.")
     return aligned_faces, image_names, no_face_images, no_face_image_names
 
 
-def augment_images(images: List[np.ndarray]) -> List[np.ndarray]:
-    augmented_images = []
-    for image in images:
-        if image is None:
-            # Skip augmentation for None images
+def augment_data(folder_path):
+    # Perform data augmentation and save the augmented images to the same folder
+    data_gen = ImageDataGenerator(
+        rotation_range=20,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode="nearest",
+    )
+
+    image_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path)]
+    for image_path in image_paths:
+        img = cv2.imread(image_path)
+        if img is None:
+            print(f"Error reading image: {image_path}")
             continue
-        image = tf.convert_to_tensor(image)
-        image = tf.image.random_flip_left_right(image)
-        image = tf.image.random_flip_up_down(image)
-        image = tf.image.random_brightness(image, max_delta=0.3)
-        image = tf.image.random_contrast(image, lower=0.7, upper=1.3)
-        image = tf.image.random_saturation(image, lower=0.7, upper=1.3)
-        image = tf.image.random_hue(image, max_delta=0.2)
-        image = image.numpy()  # Convert the Tensor back to a numpy array
 
-        augmented_images.append(image)
-
-    print(f"After augmentation, we have {len(augmented_images)} images.")
-    return augmented_images
-
-
-def load_images_with_labels(base_directory: str, target_size=(224, 224)) -> tuple:
-    """Load images from the specified directory along with their labels."""
-    images = []
-    labels = []
-    for subdir in os.listdir(base_directory):
-        subdir_path = os.path.join(base_directory, subdir)
-        if os.path.isdir(subdir_path):
-            for filename in os.listdir(subdir_path):
-                if filename.startswith(".DS_Store"):
-                    continue
-                filepath = os.path.join(subdir_path, filename)
-                # Convert HEIC to numpy array if necessary, else load image into a numpy array
-                if filepath.lower().endswith(".heic"):
-                    image = convert_heic_to_png(filepath)
-                else:
-                    image = cv2.imread(filepath)
-                    if image is None:
-                        print(f"Unable to read image: {filepath}")
-                        continue
-                    print(f"Image read: {filepath}")
-                # Resize the image to the required dimensions
-                image = resize_image(image, target_size)
-                images.append(image)
-                # Label is the name of the subdirectory
-                labels.append(subdir)
-
-    # Add "unknown" label to the labels list
-    for i in range(len(images) - len(labels)):
-        labels.append("unknown")
-
-    # Check if the number of images and labels match
-    if len(images) != len(labels):
-        print("Error: Number of images and labels do not match!")
-        print(f"Loaded {len(images)} images and {len(labels)} labels.")
-        for subdir in os.listdir(base_directory):
-            subdir_path = os.path.join(base_directory, subdir)
-            if os.path.isdir(subdir_path):
-                for filename in os.listdir(subdir_path):
-                    if filename.startswith(".DS_Store"):
-                        continue
-                    filepath = os.path.join(subdir_path, filename)
-                    if filepath not in labels:
-                        print(f"Missing label for image: {filepath}")
-        raise AssertionError("Number of images and labels do not match!")
-
-    print(f"Loaded {len(images)} images and {len(labels)} labels.")
-    return images, labels
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = img.reshape((1,) + img.shape)  # Convert to 4D tensor
+        i = 0
+        for batch in data_gen.flow(
+            img,
+            batch_size=1,
+            save_to_dir=folder_path,
+            save_prefix="aug",
+            save_format="jpg",
+        ):
+            i += 1
+            if i >= 10:  # Generate 10 augmented images for each original image
+                break
 
 
-def resize_image(image: np.ndarray, size=(224, 224)) -> np.ndarray:
-    """Resize an image to the specified size."""
-    return cv2.resize(image, size)
+if __name__ == "__main__":
+    data_folder = "data"
+    yunet_model_path = "models/face_detection_yunet_2022mar.onnx"
+
+    for person_folder in os.listdir(data_folder):
+        person_folder_path = os.path.join(data_folder, person_folder)
+        if os.path.isdir(person_folder_path):
+            # Align faces in the person's folder using YUNET
+            align_faces_in_directory(person_folder_path, yunet_model_path)
+
+            # Perform data augmentation on the aligned faces
+            augment_data(person_folder_path)
