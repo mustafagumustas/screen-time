b93bee16fedcc30810c1b6e24ab5be3e695bef79
Mustafa Gümüştaş <mustafagumustas6@gmail.com>
2023-10-11 22:33:55 +0300

    frame extraction only with the ones which has face in them

--- scripts/video_processing.py
+++ scripts/video_processing.py
@@ -1,6 +1,11 @@
 import cv2
-import time
+import dlib
 import os
+import time
+import multiprocessing
+
+predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
+detector = dlib.get_frontal_face_detector()
 
 
 def extract_frames(video_path, output_dir):
@@ -16,9 +21,7 @@ def extract_frames(video_path, output_dir):
     os.makedirs(output_dir, exist_ok=True)
 
     frame_count = 0
-
-    # getting fps info from the video
-    fps = cap.get(cv2.CAP_PROP_FPS)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
 
     while True:
         # Read a frame from the video
@@ -28,18 +31,32 @@ def extract_frames(video_path, output_dir):
         if not ret:
             break
 
-        # Save one frame per second
+        # Save frames with detected faces at the specified interval
         if frame_count % fps == 0:
-            output_file = os.path.join(output_dir, f"frame_{frame_count:04d}.jpg")
-            cv2.imwrite(output_file, frame)
+            faces = detector(frame)
+            if len(faces) >= 1:
+                output_file = os.path.join(output_dir, f"frame_{frame_count:04d}.jpg")
+                cv2.imwrite(output_file, frame)
 
         frame_count += 1
 
     # Release the video capture object
     cap.release()
 
-    print(f"Frames extracted: {frame_count}")
-    print(f"Frames saved in: {output_dir}")
+    print(f"Frames with faces extracted: {frame_count}")
+    print(f"Frames with faces saved in: {output_dir}")
+
+
+def face_finder(frame_path):
+    image = cv2.imread(frame_path)
+    rects = detector(image, 0)
+    print(rects)
+
+
+def non_face_remover(frame_path):
+    frames = [
+        os.path.join(output_directory, frame) for frame in os.listdir(output_directory)
+    ]
 
 
 if __name__ == "__main__":
@@ -47,5 +64,6 @@ if __name__ == "__main__":
     output_directory = "output_faces"  # Replace with your desired output directory
     start_time = time.time()
     extract_frames(video_path, output_directory)
+    # face_finder(output_directory)
     end_time = time.time()
     print(f"Time taken: {end_time - start_time:.2f} seconds")
