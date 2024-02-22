fcad5fa2c0ccc0009ab42f665bea9e2440241519
Mustafa Gümüştaş <mustafagumustas6@gmail.com>
2024-02-23 00:29:52 +0300

    changed the way of input taking

--- scripts/yt_playlist.py
+++ scripts/yt_playlist.py
@@ -1,10 +1,15 @@
-# BEGIN: e4f1a2d3gkq5
 import re
+import sys
 from pytube import YouTube, Playlist
 import subprocess
 
+# Check if a URL is provided as a command-line argument
+if len(sys.argv) > 1:
+    playlist_url = sys.argv[1]
+else:
+    print("No playlist URL provided.")
+    sys.exit(1)
 
-playlist_url = input("Enter the YouTube playlist URL: ")
 p = Playlist(playlist_url)
 
 for url in p.video_urls:
@@ -16,12 +21,7 @@ for url in p.video_urls:
         subprocess.run(
             [
                 "python",
-                "/Users/mustafagumustas/screen-time/scripts/video_processing.py",
+                "scripts/video_processing.py",
                 url,
             ]
         )
-
-# END: e4f1a2d3gkq5
-
-
-# 19
